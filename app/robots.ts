c08f9789bdb632fa2a642dd233b9e67732fb0825
jules-vt <jules.viot2@gmail.com>
2026-03-04 09:27:29 -0500

    feat: add app-ads.txt for ad management and update robots.txt to disallow access

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,14 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
 
-export const dynamic = 'force-static'
+export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
+      userAgent: "*",
+      allow: "/",
+      disallow: "/app-ads.txt",
     },
-    sitemap: 'https://edge-iptv.app/sitemap.xml',
-  }
+    sitemap: "https://edge-iptv.app/sitemap.xml",
+  };
 }
